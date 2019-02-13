c6fc0afdea78543b10fde31252cd79bcb7e8f063
Elvis Knapman <elvis@elvisknapman.com>
2019-02-13 17:44:34 -0500

    Handle error if API returns false, clear movie info from DOM on each event handler call

--- app.js
+++ app.js
@@ -18,20 +18,46 @@ document.getElementById('submit').addEventListener('click', (event) => {
     }
     
     xhr.send();
+
+    document.getElementById('movie-title-search').value = '';s
 });
 
 
 
-const parseResponse = function(response) {
+const parseResponse = (response) => {
     // parse the JSON response data to an object
     const movie = JSON.parse(response);
-    document.getElementById('poster').innerHTML = `<img src="${movie.Poster}">`;
-    document.getElementById('title-year').innerHTML = `${movie.Title} (${movie.Year})`;
-    document.getElementById('critic-rating').innerHTML = `${movie.imdbRating}/10`;
-    document.getElementById('rated').innerHTML = `${movie.Rated}`;
-    document.getElementById('genre').innerHTML = `${movie.Genre}`;
-    document.getElementById('runtime').innerHTML = `${movie.Runtime}`;
-    document.getElementById('release-date').innerHTML= `${movie.Released}`;
-    document.getElementById('starring').innerHTML = `${movie.Actors}`;
 
+    // remove all error messaging every time event handler is called
+    document.getElementById('status').innerHTML = '';
+
+    // remove all movie info every time event handler is called
+    removeMovieInfo();
+
+    // if API sends back a property called 'Response' and it is False, it didn't find any results
+    if(movie.Response === 'False') {
+        let errorMsg = document.createElement('p');
+        errorMsg.innerHTML = 'Sorry, your search did not return any results.';
+        document.getElementById('status').appendChild(errorMsg);
+    } else {
+        document.getElementById('poster').innerHTML = `<img src="${movie.Poster}">`;
+        document.getElementById('title-year').innerHTML = `${movie.Title} (${movie.Year})`;
+        document.getElementById('critic-rating').innerHTML = `${movie.imdbRating}/10`;
+        document.getElementById('rated').innerHTML = `${movie.Rated}`;
+        document.getElementById('genre').innerHTML = `${movie.Genre}`;
+        document.getElementById('runtime').innerHTML = `${movie.Runtime}`;
+        document.getElementById('release-date').innerHTML= `${movie.Released}`;
+        document.getElementById('starring').innerHTML = `${movie.Actors}`;
+    }
+}
+
+const removeMovieInfo = () => {
+    document.getElementById('poster').innerHTML = '';
+    document.getElementById('title-year').innerHTML = '';
+    document.getElementById('critic-rating').innerHTML = '';
+    document.getElementById('rated').innerHTML = '';
+    document.getElementById('genre').innerHTML = '';
+    document.getElementById('runtime').innerHTML = '';
+    document.getElementById('release-date').innerHTML= '';
+    document.getElementById('starring').innerHTML = '';
 }
